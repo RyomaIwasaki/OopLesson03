1c62c0d08c3d9d9fdfa6ee5dd78d13af95944f42
RyomaIwasaki <31120@ojs.ac.jp>
2020-10-09 14:23:03 +0900

    問題6-1

--- Chapter6/Program.cs
+++ Chapter6/Program.cs
@@ -8,44 +8,37 @@ using System.Threading.Tasks;
 namespace Chapter6 {
     class Program {
         static void Main(string[] args) {
-            //整数の例
-            var numbrs = new List<int> { 19, 17, 15, 24, 12, 17, 14, 20, 12, 28, 19, 30, 24 };
-
-            //var strings = numbrs.Select(n => n.ToString("0000")).ToArray();
-            //foreach (var str in strings)
-            //{
-            //    Console.Write(str + "  ");
-            //}
-
-            numbrs.Select(n => n.ToString("0000")).ToList().ForEach(s => Console.Write(s + " "));
-            //並べ替え
-            var sortNumber = numbrs.OrderBy(n => n);
-            foreach (var nums in sortNumber)
+            //6.1
+            var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
+            //6.1.1
+            Console.WriteLine(numbers.Max(num => num));
+            Console.WriteLine();
+            //6.1.2
+            var results = numbers.Length - 2;
+            foreach (var item in numbers.Skip(results))
             {
-                Console.WriteLine(nums + " ");
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+            //6.1.3
+            var ress = numbers.Select(n => n.ToString("000")).ToList();
+            foreach (var item in ress)
+            {
+                Console.WriteLine(item);
+
             }
             Console.WriteLine();
-
-
-            //文字列の例
-            var words = new List<string> {
-                "Microsoft","Apple","Google","Oracle","FaceBook",};
-            var lower = words.Select(name => name.ToLower()).ToArray();
-            //オブジェクトの例
-            var books = Books.GetBooks();
-            //タイトルリスト
-            var titles = books.Select(name=>name.Title);
-            foreach (var title in titles)
+            //6.1.4
+            var sort=numbers.OrderBy(n=>n).Take(3);
+            foreach (var s in sort)
             {
-                Console.WriteLine(title + "  ");
+                Console.WriteLine(s);
             }
             Console.WriteLine();
-            //ページ数の多い順に並べ替え（または金額の高い順）           
-            var sortBooks = books.OrderByDescending(book=>book.Pages);
-            foreach (var book in sortBooks)
-            {
-                Console.WriteLine(book.Title + " "+book.Pages);
-            }
+            //6.1.5
+            var counts = numbers.Distinct().Count(n => n > 10);
+            Console.WriteLine($"10以上の数は{counts}個です");
             Console.WriteLine();
 
         }
