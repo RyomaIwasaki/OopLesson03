70777457579403287930b4a1173fbc2bb6ff7f18
RyomaIwasaki <31120@ojs.ac.jp>
2020-10-15 11:56:15 +0900

    Chapter7 演習

--- Chaper7/Program.cs
+++ Chaper7/Program.cs
@@ -9,42 +9,58 @@ using System.Threading.Tasks;
 namespace Chapter7 {
     class Program {
         static void Main(string[] args) {
-            //var lines = File.ReadAllLines("sample.txt");
-            //var we = new WordsExtractor(lines);
-            //foreach (var word in we.Extract())
-            //{
-            //    Console.WriteLine(word);
-            //}
-            DuplicateKeySample();
-        }
+            Console.WriteLine("**********************");
+            Console.WriteLine("* 辞書登録プログラム *");
+            Console.WriteLine("**********************");
+            int num;
+            var dict = new Dictionary<string, List<string>>();
 
-        static public void DuplicateKeySample() {
-            // ディクショナリの初期化
-            var dict = new Dictionary<string, List<string>>() {
-               { "PC", new List<string> { "パーソナル コンピュータ", "プログラム カウンタ", } },
-               { "CD", new List<string> { "コンパクト ディスク", "キャッシュ ディスペンサー", } },
-            };
-
-            // ディクショナリに追加
-            var key = "EC";
-            var value = "電子商取引";
-            if (dict.ContainsKey(key))
+            do
             {
-                dict[key].Add(value);
-            }
-            else
-            {
-                dict[key] = new List<string> { value };
-            }
-
-            // ディクショナリの内容を列挙
-            foreach (var item in dict)
-            {
-                foreach (var term in item.Value)
+                Console.WriteLine("1．登録　2．内容を表示　3．終了");
+                Console.Write(">");
+                var flag = Console.ReadLine();
+                Console.WriteLine();
+                if (int.TryParse(flag, out num))
                 {
-                    Console.WriteLine("{0} : {1}", item.Key, term);
+                    switch (num)
+                    {
+                        case 1:
+                            // ディクショナリに追加
+                            Console.Write("KEYを入力:");
+                            var key = Console.ReadLine();
+                            Console.Write("VALUEを入力:");
+                            var value = Console.ReadLine();
+                            if (dict.ContainsKey(key))
+                            {
+                                dict[key].Add(value);
+                            }
+                            else
+                            {
+                                dict[key] = new List<string> { value };
+                            }
+                            Console.WriteLine("登録しました");
+                            break;
+                        case 2:
+                            // ディクショナリの内容を列挙
+                            foreach (var item in dict)
+                            {
+                                foreach (var term in item.Value)
+                                {
+                                    Console.WriteLine("{0} : {1}", item.Key, term);
+                                }
+                            }
+                            break;
+                        case 3:
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            break;
+                    }
                 }
-            }
+            } while (true);
         }
+
+        
     }
 }
