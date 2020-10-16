b5f56e41bde6d4e0f3af3896ec13251dcc6d3987
RyomaIwasaki <31120@ojs.ac.jp>
2020-10-16 15:07:26 +0900

    問題7.1

--- Chaper7/Program.cs
+++ Chaper7/Program.cs
@@ -9,58 +9,27 @@ using System.Threading.Tasks;
 namespace Chapter7 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("**********************");
-            Console.WriteLine("* 辞書登録プログラム *");
-            Console.WriteLine("**********************");
-            int num;
-            var dict = new Dictionary<string, List<string>>();
-
-            do
-            {
-                Console.WriteLine("1．登録　2．内容を表示　3．終了");
-                Console.Write(">");
-                var flag = Console.ReadLine();
-                Console.WriteLine();
-                if (int.TryParse(flag, out num))
-                {
-                    switch (num)
-                    {
-                        case 1:
-                            // ディクショナリに追加
-                            Console.Write("KEYを入力:");
-                            var key = Console.ReadLine();
-                            Console.Write("VALUEを入力:");
-                            var value = Console.ReadLine();
-                            if (dict.ContainsKey(key))
-                            {
-                                dict[key].Add(value);
-                            }
-                            else
-                            {
-                                dict[key] = new List<string> { value };
-                            }
-                            Console.WriteLine("登録しました");
-                            break;
-                        case 2:
-                            // ディクショナリの内容を列挙
-                            foreach (var item in dict)
-                            {
-                                foreach (var term in item.Value)
-                                {
-                                    Console.WriteLine("{0} : {1}", item.Key, term);
-                                }
-                            }
-                            break;
-                        case 3:
-                            Environment.Exit(0);
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            } while (true);
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+            Exercise1_1(text);//問題7.1.1
         }
 
-        
+        static void Exercise1_1(string text) {
+            var dict = new SortedDictionary<char, int>();
+            foreach (var ch in text) {
+                char upc = char.ToUpper(ch);
+                if ('A' <= upc && upc <= 'Z') {
+                    if (dict.ContainsKey(upc)) {
+                        dict[upc]++;
+                    }
+                    else {
+                        dict[upc] = 1;
+                    }
+                }
+            }
+
+            foreach (var item in dict) {
+                Console.WriteLine("{0}:{1}", item.Key, item.Value);
+            }
+        }
     }
 }
