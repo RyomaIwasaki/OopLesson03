17c9001aebe4768aeec3f0b55e8dd3dad184a217
RyomaIwasaki <31120@ojs.ac.jp>
2020-11-05 10:20:43 +0900

    CC,Bcc修正

--- SendMailApp/MainWindow.xaml.cs
+++ SendMailApp/MainWindow.xaml.cs
@@ -44,8 +44,14 @@ namespace SendMailApp {
             try {
                 MailMessage msg = new MailMessage("ojsinfosys01@gmail.com", tbTo.Text);
 
-                msg.CC.Add(tbCc.Text);
-                msg.Bcc.Add(tbBcc.Text);
+                if (tbCc.Text!="") {
+                    msg.CC.Add(tbCc.Text);
+
+                }
+                else if (tbBcc.Text != "") {
+                    msg.Bcc.Add(tbBcc.Text);
+                }
+
                 msg.Subject = tbTitle.Text;//件名
                 msg.Body = tbBody.Text;//本文
                 
